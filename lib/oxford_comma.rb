086a26b19164c7edb1fc0ed9e73f00e3382f5fde
Mark Bihag <markbihag330@gmail.com>
2019-09-05 05:00:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  counter = 0
+  string = ""
+  while counter <= array.size - 1 do
+    if array.size == 1
+      string = array[counter]
+    elsif counter < array.size
+      string += "#{array[counter]}, "
+    else
+      string += "and #{array[counter]}"
+    end
+    counter += 1
+  end
+  return string
 end
